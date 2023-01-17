e3bf7bc3fb41086a5ed765588a6d95f568f3d8a5
Joon Park <joonqkr@berkeley.edu>
2023-01-17 12:47:00 -0500

    get songs from selected playlist

--- src/pages/final/Final.jsx
+++ src/pages/final/Final.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
 import Logout from "../../components/logout/Logout";
 import Playlist from "../../components/playlist/Playlist";
+import axios from "axios";
 import "./final.css"
 
 export default function Final({ playlist }) {
-    /* Create pomodoro playlist. */
-    const createPomodoro = () => {
+    /* User token. */
+    const token = window.localStorage.getItem("token");
 
-    };
+    const [pomodoro, setPomodoro] = useState(null);
+
+    /* Get user's playlists. */
+    useEffect(() => {
+        /* Create pomodoro playlist. */
+        const createPomodoro = async (playlist) => {
+            /* Get songs of playlist. */
+            axios.get(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                params: {
+                    fields: 'items(track(id, uri))'
+                }
+            })
+            .then((response) => {
+                console.log(response.data);
+            }, (error) => {
+                console.log(error);
+            });
+
+        };
+        createPomodoro(playlist);
+
+    }, [playlist, token]);
 
     return (
         <div className="final">
